c23936ddad7a663ccb9ec6f741fa5f60b76023dc
Troy Oster <toster1011@me.com>
2020-03-18 18:52:28 -0600

    Started step 3

---
+++ project/src/main/java/Listener.java
@@ -0,0 +1,19 @@
+public class Listener implements GListener {
+
+    // Constructor
+    public Listener() {
+
+    }
+
+    @Override
+    public void enterProgram(GParser.ProgramContext ctx) {
+
+    }
+
+    @Override
+    public void exitProgram(GParser.ProgramContext ctx) {
+        
+    }
+
+    
+}
--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -1,19 +1,26 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
-import java.io.FileWriter;
-import java.util.ListIterator;
+// import java.util.List;
+// import java.io.FileWriter;
+// import java.util.ListIterator;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.*;
-import java.io.FileWriter;
+// import org.antlr.v4.runtime.*;
+// import java.io.FileWriter;
+import org.antlr.v4.runtime.ParserRuleContext;
 
 public class Main {
-    public static void main(String[] args) {
-        // String INPUT_FILE = "./inputs/" + args[0];
+    public static void main(String[] args) throws IOException {
+
+        // Variable declarations
         String INPUT_FILE = args[0];
         FileInputStream fileInputStream;
+        GLexer lexer;
+        GParser parser;
+        CommonTokenStream tokenStream;
+
+        // Initialize the FileInputStream object.
         try {
             fileInputStream = new FileInputStream(INPUT_FILE);
         } catch (FileNotFoundException e) {
@@ -22,18 +29,19 @@ public class Main {
             return;
         }
 
-        GLexer lexer;
-        GParser parser;
-
+        // Initialize the lexer, parser, and token stream.
         try {
             lexer = new GLexer(CharStreams.fromFileName(INPUT_FILE));
-            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            tokenStream = new CommonTokenStream(lexer);
             parser = new GParser(tokenStream);
         } catch (IOException e) {
             e.printStackTrace();
+            fileInputStream.close();
             return;
         }
 
         parser.program();
+
+        fileInputStream.close();
     }
 }
