63878a2b9c473fe4db1b0c52113fd454822336c4
Henry Soule <hsoule427@gmail.com>
2020-03-19 18:00:42 -0600

    Fixed merge conflict

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -1,17 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-<<<<<<< HEAD
-import java.util.List;
-import java.io.FileWriter;
-import java.util.ListIterator;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-=======
-
->>>>>>> ad02f84395e0b51394a9a1b1c08fdad65ba89584
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
@@ -38,14 +27,7 @@ public class Main {
             return;
         }
 
-<<<<<<< HEAD
-        GLexer lexer;
-        GParser parser;
-        GListener listener;
-
-=======
         // Initialize the lexer, parser, and token stream.
->>>>>>> ad02f84395e0b51394a9a1b1c08fdad65ba89584
         try {
             lexer = new GLexer(CharStreams.fromFileName(INPUT_FILE));
             tokenStream = new CommonTokenStream(lexer);
@@ -57,18 +39,11 @@ public class Main {
             return;
         }
 
-<<<<<<< HEAD
-        ParserRuleContext tree = parser.program();
-        ParseTreeWalker walker = new ParseTreeWalker();
-
-        walker.walk(listener, tree);
-=======
         // Initialize the tree, listener, and walker
         tree = parser.program();
         listener = new Listener();
         walker = new ParseTreeWalker();
         walker.walk(listener, tree);
         fileInputStream.close();
->>>>>>> ad02f84395e0b51394a9a1b1c08fdad65ba89584
     }
 }
